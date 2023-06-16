328d249f6927a5a74b1b96df0201549a41d0a730
abrarazmain <ajmainabrar5@gmail.com>
2023-06-16 23:01:44 +0600

    Add .gitignore to exclude node_modules

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const sortBy = require("sort-by");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,10 +9,40 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+const uri =
+  "mongodb+srv://summerSchool:8cu4CJymzYtc3CRl@cluster0.wa4fr1c.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  const classCollection = client.db("summerDB").collection("classes");
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
+    //   add class
+    app.post("/classes", async (req, res) => {
+      const newClass = req.body;
+      const result = await classCollection.insertOne(newClass);
+      res.send(result);
+    });
 
-
-
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("toys is running");
