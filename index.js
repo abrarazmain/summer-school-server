424b0182f57fea8840f88221446e6e309bf054b4
abrarazmain <ajmainabrar5@gmail.com>
2023-06-17 20:30:49 +0600

    added env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const sortBy = require("sort-by");
 require("dotenv").config();
@@ -10,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 const uri =
-  "mongodb+srv://summerSchool:8cu4CJymzYtc3CRl@cluster0.wa4fr1c.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.FIREBASE_NAME}:${process.env.FIREBASE_PUSS}@cluster0.wa4fr1c.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
